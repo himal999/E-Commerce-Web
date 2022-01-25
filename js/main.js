bb6e3880b7e352531edbb14fb1ac740a1bf5214e
himal999 <himal.official99@gmail.com>
2022-01-25 12:48:21 +0530

    Replace New Header Design

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#search,.fa-search').mouseenter(function(){
-        $('.logo').hide();
-    });
-    $('#search,.fa-search').mouseleave(function(){
-        $('.logo').show();
-    });
-});
